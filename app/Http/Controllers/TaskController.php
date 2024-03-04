02d74ff24e0c329e31c51f4eb66e94e1f40744d1
EstebanCastillo <ncastillo308@unab.edu.co>
2024-03-04 09:43:51 -0500

    Programa acerca ToDoList

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // $task['tasks'] = Task::all();
+        $tasks = Task::all();
+        return view('Task.index' , compact('tasks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+
+        // $task = new Task();
+        // $task->task = $request->task;
+        // $task->save();
+
+        Task::create($request->all());
+        return redirect('/');
+    }
+
+
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+        return redirect('/');
+    }
+}
